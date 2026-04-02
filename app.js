35703e06f768e1e8d16e0c7cc7aa792a8b636d55
kunalSingh026 <skunalkumar759@gmail.com>
2026-04-02 12:54:29 +0530

    Add subtraction feature and tests

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 function sum(a, b) {
     return a + b;
 }
-module.exports = sum;
+
+function subtract(a, b) {
+    return a - b;
+}
+
+module.exports = { sum, subtract };
--- app.test.js
+++ app.test.js
@@ -1,4 +1,7 @@
-const sum = require('./app');
+const { sum, subtract } = require('./app');
 test('adds 1 + 2 to equals 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+test('subtracts 5 - 2 to equals 3', () => {
+    expect(subtract(5, 2)).toBe(3);
 });
